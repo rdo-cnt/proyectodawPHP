f512c0ddbad1475e7257ab2e9c6f83039b66dbde
Luis Juan Sanchez <luisjuansp@gmail.com>
2015-11-24 00:46:37 -0600

    junto todo

--- mysql.php
+++ mysql.php
@@ -7,18 +7,39 @@
  */
 
 $host = "localhost";
-$username = "root";
-$password = "";
-$database = "usuarios";
+$dbusername = "root";
+$dbpassword = "";
+$database = "proyectodaw";
+$table = "user";
+
+$mysqli = new mysqli($host, $dbusername, $dbpassword, $database);
+if ($mysqli->connect_errno) {
+	echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+}
 
 
-    $mysqli = new mysqli($host, $username, $password, $database);
-    if ($mysqli->connect_errno) {
-        echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-    }
-    $query = "select * from registrados";
-    $result = $mysqli->query($query);
+function login($username, $password){
+	
+	$query = "SELECT * FROM " . $table . " WHERE `username` = '" . $username . "' AND `password` = '" . $password . "'";
 
+	if ($result = mysqli_query($mysqli, $query)) {
+        // Return the number of rows in result set
+		$rowcount = mysqli_num_rows($result);
+        //printf("Result set has %d rows.\n", $rowcount);
 
-    $mysqli -> close();
-    echo "funciono";
+		$row = mysqli_fetch_row($query);
+		$userId = $row[0];
+
+		if ($rowcount != 0) {
+			$_SESSION['username'] = $username;
+			$_SESSION['id'] = $userId;
+			header('Location: index.php');
+
+		} else {
+			echo "Usuario o password incorrecta";
+		}
+
+        // Free result set
+		mysqli_free_result($result);
+	}
+}
